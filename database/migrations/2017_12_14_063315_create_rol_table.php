2ef4e2e75c779f0b5743582891282ef7d130e022
Eduard <a>
2017-12-13 23:36:29 -0800

    [RH-1]Create migrations from user and rol

---
+++ database/migrations/2017_12_14_063315_create_rol_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rol', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->bigIncrements('id');
+            $table->string('name',25)->unique('name');
+            $table->string('create_user');
+            $table->date('create_date');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rol');
+    }
+}
--- database/migrations/2017_12_14_063356_create_user_table.php
+++ database/migrations/2017_12_14_063356_create_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,12 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->engine = 'InnoDB';
+            $table->bigIncrements('id');
+            $table->string('name',15);
+            $table->string('last_name',15);
+            $table->string('email',25)->unique('email');
+            $table->string('password',25);
+            $table->bigInteger('rol_id')->unsigned();
+            $table->foreign('rol_id')->references('id')->on('rol');
+
         });
     }
 
